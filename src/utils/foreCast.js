bc35a65f48c966e133ac312180638dd95b8fb0a3
gurungitachi <gurung.biishal88@gmail.com>
2020-12-21 22:36:07 +0545

    add wind speed and sunset at forecast

--- src/utils/foreCast.js
+++ src/utils/foreCast.js
@@ -16,7 +16,7 @@ const foreCast = (lat, lon, callBack) => {
     } else {
       callBack(
         undefined,
-        `${body.data[0].weather.description}. It is currently ${body.data[0].temp}. There is ${body.data[0].precip} % chance of rainfall`
+        `${body.data[0].weather.description}. It is currently ${body.data[0].temp}. There is ${body.data[0].precip} % chance of rainfall.The windspeed is ${body.data[0].wind_spd}. The sun will set at ${body.data[0].sunset} PM`
       );
     }
   });
